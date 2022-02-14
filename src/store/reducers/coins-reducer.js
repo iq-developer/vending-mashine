5ad4e806c7d3b530c422c22a3087303e981187fb
Vladimir Sevastianov <108.nimay@gmail.com>
2022-02-14 11:54:01 +0200

    fix: return id

--- src/store/reducers/coins-reducer.js
+++ src/store/reducers/coins-reducer.js
@@ -4,36 +4,43 @@ import userMoney from "../../data/userMoney.json";
 
 const defaultMoney = [
   {
+    id: 1,
     value: 0.01,
     name: "1p",
     quantity: 0,
   },
   {
+    id: 2,
     value: 0.02,
     name: "2p",
     quantity: 4,
   },
   {
+    id: 3,
     value: 0.05,
     name: "5p",
     quantity: 6,
   },
   {
+    id: 4,
     value: 0.10,
     name: "10p",
     quantity: 3,
   },
   {
+    id: 5,
     value: 0.20,
     name: "20p",
     quantity: 5,
   },
   {
+    id: 6,
     value: 1,
     name: "£1",
     quantity: 7,
   },
   {
+    id: 7,
     value: 2,
     name: "£2",
     quantity: 1,
--- src/store/reducers/deposited-coins-reducer.js
+++ src/store/reducers/deposited-coins-reducer.js
@@ -1,35 +1,42 @@
 const defaultMoney = [
   {
+    id: 1,
     value: 0.01,
     name: "1p",
     quantity: 0,
   },
   {
+    id: 2,
     value: 0.02,
     name: "2p",
     quantity: 0,
   },
   {
+    id: 3,
     value: 0.05,
     name: "5p",
     quantity: 0,
   },
   {
+    id: 4,
     value: 0.10,
     name: "10p",
     quantity: 0,
   },
   {
+    id: 5,
     value: 0.20,
     name: "20p",
     quantity: 0,
   },
   {
+    id: 6,
     value: 1,
     name: "£1",
     quantity: 0,
   },
   {
+    id: 7,
     value: 2,
     name: "£2",
     quantity: 0,
